b37443f1f8494df8e486ab38d9ecc6ee32c20c9d
Thomas Lin <ithomas.lin@me.com>
2014-09-23 15:59:10 +0800

    Initial Commit

---
+++ ShakeScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShakeScroll
+//
+//  Created by Thomas Lin on 9/23/14.
+//  Copyright (c) 2014 Thomas Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
