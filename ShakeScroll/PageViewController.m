ae4a1aba79ba19f5adab3eb8589077dd0f8815b7
Thomas Lin <ithomas.lin@me.com>
2014-09-24 11:44:25 +0800

    Remove pageTitle frame.

--- ShakeScroll/PageViewController.m
+++ ShakeScroll/PageViewController.m
@@ -21,10 +21,6 @@
     self.view.backgroundColor = UIColorFromRGB(0x042C3AD);
     self.pageTitle.text = self.titleText;
     self.pageTitle.textColor = [UIColor whiteColor];
-    self.pageTitle.frame = CGRectMake(self.view.frame.size.width / 2,
-                                      self.view.frame.size.height / 2,
-                                      self.view.frame.size.width,
-                                      self.view.frame.size.height);
 }
 
 - (void)didReceiveMemoryWarning {
